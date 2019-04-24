02862870338ec785972f0b4e222538fc18feafb9
DESKTOP-42P6MA3\adriandev.me <adriandev.me@gmail.com>
2019-04-24 20:31:14 +0200

    Working on main script refactoring

--- src/index.ts
+++ src/index.ts
@@ -10,10 +10,10 @@ class JobOScraper extends Command {
   static description = 'describe the command here'
 
   static flags = {
-    // URL mode, 
-    url: flags.string({char: 'u', description: 'URL MODE: URL to scrap'}),
-    // WORKER mode
-    conf: flags.string({char: 'w', description: 'WORKER MODE: Path to the configuration file'}),
+    // URL, 
+    url: flags.string({char: 'u', description: 'URL: URL to scrap'}),    
+    // CONFIG,
+    conf: flags.string({char: 'c', description: 'CONFIGURATION FILE: Filepath to the configuration file '}),
   }
 
   static args = [{name: 'file'}]
@@ -21,28 +21,32 @@ class JobOScraper extends Command {
   async run() {
     const {args, flags} = this.parse(JobOScraper)
 
+    // Init ddbb
+    await storage.init(); //@TODO should init with a proper config
+
     if (flags.url){
       // Run in URL mode
-      this.log('Running in URL mode...', flags.url);
-      // Init ddbb
-      await storage.init(); //@TODO should init with a proper config
-      // Request to the server
-      const response = await axios.get(flags.url as string);      
-      let workana = new Workana.Workana({});
-      const offers = await workana.parseHTML(response.data as any);      
+      this.log('Reading single URL...', flags.url);
+      // Get offers
+      const offers = await this.processURL(flags.url)
       // Save results
-      let e=0;
-      for (const offer of offers){
-        let o = await offer.upsert();
-        if (o)
-          e++; //@TODO waat
-      }
-      //const found = await Offer.upsertAll(offers);      
-      this.log(`Success! ${e} new offer(s) found from Workana site`);
+      const saved = await this.saveOffers(offers);
+      // Log
+      this.log(`Success! ${offers.length} new offer(s) found from Workana site`);
     }
     else if (flags.conf){
-      // Run in WORKER mode
-      this.log('Running in WORKER mode...')
+      // Check if
+      const config = this.parseConf(flags.conf);
+      // If endless mode is enabled (default false)
+      if (config.endless){
+        // Set the timer
+        setTimeout(function () { 
+          //Iterate 
+          for (const url of config.urls){ //@TODO refactor, now assuming every url is from workana site
+            
+          }
+        }, config.delay); //Using delay from config
+      }
     }
     else{
       this.log('No param set, running in URL mode...');
@@ -54,6 +58,32 @@ class JobOScraper extends Command {
     // if (args.file && flags.force) {
     //   this.log(`you input --force and --file: ${args.file}`)
     // }
+  }
+
+  private async processURL(url:string){
+    // Init array
+    let offers:Array<Offer>=[];
+    // Request to the server
+    const response = await axios.get(url as string);      
+    let workana = new Workana.Workana({});
+    offers = await workana.parseHTML(response.data as any);      
+    // Return
+    return offers;
+  }
+
+  private async saveOffers(offers:Array<Offer>){
+    let upserted:Array<Offer>=[];  
+    for (const offer of offers){
+      let o = await offer.upsert();
+      if (o)
+        upserted.push(o);
+    }
+    return upserted;
+  }
+
+  private parseConf(raw_config: any){
+    //@TODO parse
+    return raw_config;
   }
 }
 
